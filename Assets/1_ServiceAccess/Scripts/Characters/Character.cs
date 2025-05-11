47cff07f7f2b431777c1959b567e6643fb305e68
SebastianLuser <sebastianv.08.2000@gmail.com>
2025-05-11 16:34:12 -0300

    apply design patterns to the characters

--- Assets/1_ServiceAccess/Scripts/Characters/Character.cs
+++ Assets/1_ServiceAccess/Scripts/Characters/Character.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Excercise1
@@ -6,15 +5,31 @@ namespace Excercise1
     public class Character : MonoBehaviour, ICharacter
     {
         [SerializeField] protected string id;
-
+        public string Id => id;
         protected virtual void OnEnable()
         {
             //TODO: Add to CharacterService. The id should be the given serialized field. 
+            RegisterWithService();
         }
-
+        
         protected virtual void OnDisable()
         {
             //TODO: Remove from CharacterService.
+            UnregisterFromService();
         }
+        
+        private void RegisterWithService()
+        {
+            CharacterService.Instance.TryAddCharacter(id, this);
+        }
+        
+        private void UnregisterFromService()
+        {
+            if (string.IsNullOrEmpty(id))
+                return;
+                
+            CharacterService.Instance.TryRemoveCharacter(id);
+        }
+        
     }
 }
--- Assets/1_ServiceAccess/Scripts/Characters/CharacterService.cs
+++ Assets/1_ServiceAccess/Scripts/Characters/CharacterService.cs
@@ -5,10 +5,73 @@ namespace Excercise1
 {
     public class CharacterService : MonoBehaviour
     {
+        private static CharacterService _instance;
+        
         private readonly Dictionary<string, ICharacter> _charactersById = new();
-        public bool TryAddCharacter(string id, ICharacter character)
-            => _charactersById.TryAdd(id, character);
-        public bool TryRemoveCharacter(string id)
-            => _charactersById.Remove(id);
+        
+        public static CharacterService Instance 
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<CharacterService>();
+                    
+                    if (_instance == null)
+                    {
+                        Debug.LogError("CharacterService not found in scene!");
+                    }
+                }
+                return _instance;
+            }
+        }
+        
+        private void Awake()
+        {
+            InitializeSingleton();
+        }
+        
+        private void InitializeSingleton()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            
+            _instance = this;
+        }
+
+        public void TryAddCharacter(string id, ICharacter character)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                Debug.LogError("Cannot add character with empty ID");
+                return;
+            }
+
+            _charactersById.TryAdd(id, character);
+        }
+        public void TryRemoveCharacter(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                Debug.LogWarning("Attempted to remove character with empty ID");
+                return;
+            }
+
+            _charactersById.Remove(id);
+        }
+        
+        public bool TryGetCharacter(string id, out ICharacter character)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                character = null;
+                return false;
+            }
+            
+            return _charactersById.TryGetValue(id, out character);
+        }
     }
 }
