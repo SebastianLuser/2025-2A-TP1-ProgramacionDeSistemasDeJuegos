fb66b2942c34d1e87b726432481385789691d647
SebastianLuser <sebastianv.08.2000@gmail.com>
2025-05-21 21:15:57 -0300

    movement

---
+++ Assets/1_ServiceAccess/Scripts/Characters/EnemyMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Excercise1
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+         private float _speed;
+        public void InitializeSpeed(float speed)
+        {
+            _speed = speed <= 0f ? 5f : speed;
+        }
+        public void MoveTo(Vector3 targetPosition)
+        {
+            Vector3 direction = targetPosition - transform.position;
+            transform.position += direction.normalized * (_speed * Time.deltaTime);
+        }
+    }
+}
